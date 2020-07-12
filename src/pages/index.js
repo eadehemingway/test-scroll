5a8cb0f0309a2db8c1a8dd8f3a5aec6a91b52961
eadehemingway <eade.hemingway@gmail.com>
2020-07-12 20:52:51 +0200

    move One all into index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,48 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components"
-import One from "../components/One"
 
+import scrollama from "scrollama" // or...
 import "intersection-observer"
 
 const IndexPage = () => {
+  const [isActiveIndex, setIsActiveIndex] = useState()
+
+  useEffect(() => {
+    const scroller = scrollama()
+
+    scroller
+      .setup({
+        step: ".step",
+        debug: true, // this being true is what makes their dotted line show
+        offset: 0.2, // where the dotted line shows up
+        // progress: true,
+      })
+      .onStepEnter(res => {
+        console.log("enterrrrrrrrrrr")
+        setIsActiveIndex(res.index)
+      })
+      .onStepExit(res => {
+        console.log("exxxxxxxxxit")
+        setIsActiveIndex(undefined)
+      })
+  }, [])
+  const steps = ["a", "b", "c"]
+
   return (
     <Scroll id="scroll">
-      <One />
+      <Container>
+        {steps.map((s, i) => {
+          const isActive = i === isActiveIndex
+          const background = isActive ? "pink" : "red"
+          return (
+            <Box className="step" style={{ background }} key={i}>
+              {s}
+            </Box>
+          )
+        })}
+
+        <Outro />
+      </Container>
     </Scroll>
   )
 }
@@ -15,5 +50,18 @@ const IndexPage = () => {
 const Scroll = styled.div`
   position: relative;
 `
-
+const Container = styled.div`
+  position: relative;
+`
+const Outro = styled.div`
+  height: 1000px;
+`
+const Box = styled.div`
+  min-height: 300px;
+  height: 300px;
+  width: 300px;
+  background: red;
+  margin: 200px;
+  margin-bottom: 0;
+`
 export default IndexPage
